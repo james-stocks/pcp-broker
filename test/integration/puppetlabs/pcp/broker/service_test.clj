fb79c50e0a6f3b582c996f94a6306b1ba8082615
james-stocks <james.stocks@puppetlabs.com>
2016-11-14 16:03:37 +0000

    (WIP) Attempt to put a ☃ character into broker certname

--- test/integration/puppetlabs/pcp/broker/service_test.clj
+++ test/integration/puppetlabs/pcp/broker/service_test.clj
@@ -34,8 +34,8 @@
                ;; tests.
                :ssl-port 8143
                :client-auth "want"
-               :ssl-key "./test-resources/ssl/private_keys/broker.example.com.pem"
-               :ssl-cert "./test-resources/ssl/certs/broker.example.com.pem"
+               :ssl-key "./test-resources/ssl/private_keys/broker.☃.example.com.pem"
+               :ssl-cert "./test-resources/ssl/certs/broker.☃.example.com.pem"
                :ssl-ca-cert "./test-resources/ssl/ca/ca_crt.pem"
                :ssl-crl-path "./test-resources/ssl/ca/ca_crl.pem"}
 
